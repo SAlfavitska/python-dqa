92ac32b63ff6b0a1462a70862fe3c6d573237d8e
SAlfavitska <alfavitskasolomiya61@gmail.com>
2022-02-02 15:52:17 +0200

    Added changes for renaming key with dict number with max value

--- collections_homework_2.py
+++ collections_homework_2.py
@@ -32,16 +32,18 @@ def generate_dict_with_value_list(dict_list):  # Creating the function.
     Takes in list of dicts (dict_list),
     returns the dict with key and list of value(s) for each key
     """
-    dict_with_value_list = {}  # Creating the empty list for storing key and list of values per key.
-    for each_dict in dict_list:  # Iterating through taken list of dicts.
-        for key, value in each_dict.items():  # Iterating through key, value in each dict.
-            if key in dict_with_value_list:  # Checking if the key is in dict_with_value_list.
-                dict_with_value_list[key].append(value)
+    dict_with_value_list = {}  # Creating the empty list for storing key, list of values and list if value indexes
+    for i in range(len(dict_list)):  # Iterating through length of taken list of dicts
+        for key, value in dict_list[i].items():  # Iterating through each key/value pair
+            if key not in dict_with_value_list.keys():  # Checking if the key is not in dict_with_value_list.
+                dict_with_value_list[key] = [[value], [i]]
+                # If the key isn't in dict_with_value_list then add key, lists of values and value indexes
+            elif key in dict_with_value_list.keys():  # Checking if the key is in dict_with_value_list.
                 # If the key is already in dict_with_value_list then append its value with new one.
-            else:
-                dict_with_value_list[key] = [value]
-                # If key is not in dict_with_value_list then just insert key and value as list.
-    print(f"Dict with list of value(s):\n{dict_with_value_list}")  # Print to console.
+                dict_with_value_list[key][0].append(value)
+                # Appending the index list for this value
+                dict_with_value_list[key][1].append(i)
+    print(f"Dict with list of value(s) and index(es) :\n{dict_with_value_list}")  # Print to console.
     return dict_with_value_list  # Return list_of_dicts
 
 
@@ -54,12 +56,12 @@ def dict_key_renaming_and_assigning_max_value(dict_with_value_list):  # Creating
 
     final_dict = {}  # Creating an empty dict to store the final result.
     for key, value in dict_with_value_list.items():  # Iterating through the taken dict with list of values.
-        if len(value) > 1:  # If the list of values has more than one element.
-            max_index = value.index(max(value))  # Storing in max_index the index of max value in values list.
-            final_dict[key + '_' + str(max_index + 1)] = max(value)
-            # Renaming the key with _max_index and assign the max value to it.
+        if len(value[0]) > 1:  # If the list of values has more than one element.
+            max_value = max(value[0])  # Storing max_value of the values list.
+            final_dict[key + '_' + str(value[1][value[0].index(max_value)]+1)] = max_value
+            # Renaming the key with _x there x is the number of dict with max value and assign the max value to it.
         else:
-            final_dict[key] = value[0]
+            final_dict[key] = value[0][0]
             # If the list of values is empty or has one element then assign the values to it.
     print(f"Final dict with renamed duplicate keys and max values:\n{final_dict}")   # Print to console.
     return final_dict  # Return final_dict
@@ -67,4 +69,3 @@ def dict_key_renaming_and_assigning_max_value(dict_with_value_list):  # Creating
 
 dict_key_renaming_and_assigning_max_value(generate_dict_with_value_list(generate_list_of_dicts(5, 3)))
 # Calling the function.
-
